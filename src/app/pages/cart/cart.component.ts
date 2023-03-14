b74f88a462110ebb29e626801648da0aa0077713
ndevanthery <devantherynicolas@gmail.com>
2023-03-15 00:26:24 +0100

    Stripe integration for payment

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from "@angular/common/http";
 import { Component, OnInit } from "@angular/core";
+import { loadStripe } from "@stripe/stripe-js";
 import { Cart, CartItem } from "src/app/models/cart.model";
 import { Product } from "src/app/models/product.model";
 import { CartService } from "src/app/services/cart.service";
@@ -8,7 +10,7 @@ import { CartService } from "src/app/services/cart.service";
   templateUrl: "cart.component.html",
 })
 export class CartComponent implements OnInit {
-  constructor(private _cartService: CartService) {}
+  constructor(private _cartService: CartService, private http: HttpClient) {}
   cart: Cart = {
     items: [],
   };
@@ -47,5 +49,20 @@ export class CartComponent implements OnInit {
 
   onRemoveQuantity(item: CartItem): void {
     this._cartService.removeQuantity(item);
+  }
+
+  onCheckout(): void {
+    this.http
+      .post("http://localhost:4242/checkout", {
+        items: this.cart.items,
+      })
+      .subscribe(async (res: any) => {
+        let stripe = await loadStripe(
+          "pk_test_51MlgdiK2Dl4cnWzq4y7XqTbGrqwnBbaQypa1HCf2mZmWBzTE01e5kKoHoL8G0uJS5xvSsbXeD3mf57FYPvUMm5iX00DIsK9waX"
+        );
+        stripe?.redirectToCheckout({
+          sessionId: res.id,
+        });
+      });
   }
 }
